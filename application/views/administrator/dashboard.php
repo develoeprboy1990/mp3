bee23dfc4797477f3c87b7abe4380339afeea4ea
develoeprboy1990 <62117143+develoeprboy1990@users.noreply.github.com>
2021-11-03 20:45:01 +0500

    upload the project

---
+++ application/views/administrator/dashboard.php
@@ -0,0 +1,56 @@
+            <div class="page-header">
+                <div class="page-header-title">
+                    <h4>Dashboard</h4>
+                </div>
+                <div class="page-header-breadcrumb">
+                    <ul class="breadcrumb-title">
+                        <li class="breadcrumb-item">
+                            <a href="index-2.html">
+                                <i class="icofont icofont-home"></i>
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="#!">Pages</a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="#!">Dashboard</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="page-body">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="card table-1-card">
+                                    <div class="card-block">
+                                        <div class="table-responsive">
+                                            <table class="table">
+                                                <thead>
+                                                    <tr class="text-capitalize">
+                                                        <th>Type</th>
+                                                        <th>Lead Name</th>
+                                                        <th>Views</th>
+                                                        <th>Last Date</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td><a href="#!">Buyer</a>
+                                                        </td>
+                                                        <td>Denish Ann</td>
+                                                        <td>9.23 A.M.</td>
+                                                        <td>9/27/2015</td>
+                                                    </tr>
+                                                   
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                          
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
